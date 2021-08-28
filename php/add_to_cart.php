df746c63b58ea19a76c9950d20f889278eb7d3cb
Yordan <dani.1998@abv.bg>
2021-08-28 18:11:50 +0300

    cart

--- php/add_to_cart.php
+++ php/add_to_cart.php
@@ -8,58 +8,57 @@
 		$count = $_POST['counter']; 
 
 		$newProduct = array('product_id' => $productId, 'count' => $count, 'product_color' => $productColor);
+		// var_dump($newProduct);
 
 		session_start();
-		// echo count($_SESSION['cart']);
-		//array_push($_SESSION['cart'], $newProduct);
-
-		if (count($_SESSION['cart']) == 0){
-			array_push($_SESSION['cart'], $newProduct);
-		} else if (count($_SESSION['cart']) > 0) {
-			//echo 'before for: '.count($_SESSION['cart']);
-			// echo '<br>';
-
-			// foreach ($_SESSION['cart'] as $cartIndex) {
-			// 	if(!array_search($newProduct['product_id'], $cartIndex)) {
-			// 		array_push($_SESSION['cart'], $newProduct);
-			// 		break;
-			// 	} else {
-			// 	print_r($cartIndex);
-
-			// 	// }
-			// }
-
-			// for ($i=0; $i < count($_SESSION['cart']); $i++) { 
-			// 	if(array_search($newProduct['product_id'], $_SESSION['cart'][$i])) {
-			// 		if (array_search($newProduct['product_color'], $_SESSION['cart'][$i])) {
-			// 			//echo '<br>count: '.$_SESSION['cart'][$i]['count'];
-			// 			$_SESSION['cart'][$i]['count'] += $newProduct['count'];
-			// 		} else {
-			// 			array_push($_SESSION['cart'], $newProduct);
-			// 		}
-			// 		// echo '<br>'.array_search($newProduct['product_id'], $_SESSION['cart'][$i]);
-			// 	} else {
-			// 		array_push($_SESSION['cart'], $newProduct);
-			// 	}
-
-			// 	// echo '<br>new id: '.$newProduct['product_id'];
-			// 	// echo '<br>old id: '.$_SESSION['cart'][$i]['product_id'];
-			// 	// if($newProduct['product_id'] == $_SESSION['cart'][$i]['product_id'] && $newProduct['product_color'] == $_SESSION['cart'][$i]['product_color']) {
-			// 	// 		$_SESSION['cart'][$i]['count'] += $newProduct['count'];
-
-			// 			// echo '<br>updated count: '.$_SESSION['cart'][$i]['count'];
-			// 			// return;
-			// 		// } else {
-			// 		// 	array_push($_SESSION['cart'], $newProduct);
-			// 		// }
-			// 	// } else {
-
-			// 		// array_push($_SESSION['cart'], $newProduct);
-			// 	// }
-			// 	// echo '<br>new id: '.$newProduct['product_id'];
-			// }
+		
+		if( empty( $_SESSION['cart'] ) ) {
+			$_SESSION['cart'] = array();
 		}
-	}
+
+		// array_push($_SESSION['cart'], $newProduct);
+
+	
+			echo 'before for: '.count($_SESSION['cart']);
+			echo '<br>';
+
+			$product_ids = array();
+			if ( ! empty( $_SESSION['cart'] ) ) {
+				foreach ( $_SESSION['cart'] as $product_item ) {
+					$product_ids[] = $product_item["product_id"];
+				}
+
+			}
+			var_dump($newProduct);
+
+			if( array_search($newProduct['product_id'], $product_ids ) === false ) {
+				array_push($_SESSION['cart'], $newProduct);
+			} else {
+				$is_new = true;
+				for ($i=0; $i < count($_SESSION['cart']); $i++) { 
+					if( $newProduct['product_id'] === $_SESSION['cart'][$i]['product_id'] ) {
+						if ( $newProduct['product_color'] === $_SESSION['cart'][$i]['product_color'] ) {
+							echo '<br>count: '.$_SESSION['cart'][$i]['count'];
+							$_SESSION['cart'][$i]['count'] += $newProduct['count'];
+							$is_new = false;
+							break;
+						} 
+						echo '<br>'.array_search($newProduct['product_id'], $_SESSION['cart'][$i]);
+					} 
+				}
+
+				if ( $is_new ) {
+					array_push($_SESSION['cart'], $newProduct);
+				}
+
+				echo '<br>new id: '.$newProduct['product_id'];
+				echo '<br>old id: '.$_SESSION['cart'][$i]['product_id'];
+			}
+				echo '<br>new id: '.$newProduct['product_id'];
+				print_r($_SESSION['cart']);
+			
+		}
+//	}
 	// header("location: ../product.php?product=$productId");
 ?>
 <!-- 
